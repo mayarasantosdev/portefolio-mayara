4fede8827ac3c870c886904aaf7ae10d572c777f
Mayara Santos <mayara18.santos@gmail.com>
2024-11-19 18:46:02 -0300

    alteraçao3
    alteraçao3

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,6 @@
-const el = document.querySelector("#txt");
-const text = "Estudante de Técnico em Análise e Desenvolvimento de Sistemas";
-const interval = 200;
+const texto = document.getElementById("especialidade-texto");
+const startBtn = document.getElementById("startAnimationBtn");
 
-function showtext(el, text, interval){
-    const char = text.split("").reverse();
-    
-    const typer = setInterval(() => {
-        if(!char.length) {
-            return clearInterval(typer);
-        }
-        const next = char.pop();
-        el.innerHTML += next;
-    
-    }, interval);
-
-}
-
-showtext(el, text, interval);
+startBtn.addEventListener("click", () => {
+    texto.style.transform = "translateX(-100%)"; // Move a palavra da direita para a esquerda
+});
